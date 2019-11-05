ee3ccea29413dcc8533895dde6c861088979a89a
milad440550 <milad440550@gmail.com>
2019-11-05 10:19:54 +0100

    Added movies api GET and POST

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const movieController = require('../contoller/movies');
 const router = express.Router();
+
+router.get('/', movieController.GetAllMovies);
+router.post('/add-movie', movieController.CreateMovie);
 
 module.exports = router;
